2a8166b06430593c186e6ec1f3b23e4b0a047252
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-06-15 14:45:47 -0700

    additional notes added

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-//'server.js' serves as our 'hub'. It is where all of our routes lead, so that our app can run properly.
+//'server.js' serves as our 'hub'. It is where all of our routes lead, so that our app can run properly from here. We 'require' the 'express' NodeJS framework so that we can utilize its built-in features to run our app.
 const express = require('express');
+
+//One of the built in features of 'express' is 'path'. We access it here so that our app can utilize it to make sense of the 'path's we assign to find each file, when they are referenced in the code block. Actual 'path's to each of those supplementary files are detailed where they are declared.
+const path = require('path');
 
 //Here, we are accessing the 'index.js' file in the 'routes' folder of our file tree. We do not need to specifically name 'index.js' as the file, because JavaScript (by default) looks for the file by that name in the folder we've specified.
 const routes = require('./routes');
@@ -7,7 +10,7 @@ const routes = require('./routes');
 //Here, we let our app know that it will be receiving all 'process'ing activity either through the Heroku 'env'ironment (for this app), or through our local 'port' '3001'.
 const PORT = process.env.port || 3001;
 
-//Here, we that we initialize 'express' so that we can enable our routes and let our files exchange information.
+//Here, we initialize 'express' (which we're defining as 'app') so that we can enable our routes and let our files exchange information.
 const app = express();
 
 app.use(express.json());
